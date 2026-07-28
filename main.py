d6c11f9156ef39d984d32da16118bd64ba494c52
Frogghev <desgiovanni1@gmail.com>
2026-07-28 16:32:57 +0200

    merging branches

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+#Script per scaricare i dati dal sito istat dal 2020 in poi
 import requests
 
 url = 'https://esploradati.istat.it/SDMXWS/rest/data/41_983'
 header = {'Accept': 'application/vnd.sdmx.data+csv;version=1.0.0'}
 params = {
-    'startPeriod' : '2020'
+    'startPeriod' : 2020
 }
 
 data = requests.get(url, headers=header, params=params, stream=True)
