9793628117036ddc2ffbb171ffc7ee1ec22df300
Frogghev <desgiovanni1@gmail.com>
2026-07-28 16:10:03 +0200

    finalized the script to aquire the data from the istat site

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import requests
 
-#url = "https://microsoftedge.github.io/Demos/json-dummy-data/64KB.json"
 url = 'https://esploradati.istat.it/SDMXWS/rest/data/41_983'
+header = {'Accept': 'application/vnd.sdmx.data+csv;version=1.0.0'}
+params = {
+    'startPeriod' : '2020'
+}
 
-print("starting the script")
-data = requests.get(url, headers={'Accept': 'application/vnd.sdmx.data+csv;version=1.0.0'}, stream=True)
-print('starting scraping of data')
+data = requests.get(url, headers=header, params=params, stream=True)
 data.raise_for_status()
+
+with open("database_car_accident.csv", 'w') as file:
+    file.write(data.text)
+
 
 
