7d9411e8f1bc5b13cacff3b497f31afc1071f7d2
Giovanni De Santis <desgiovanni1@gmail.com>
2026-07-30 11:59:23 +0200

    Analysis of null values

--- main.py
+++ main.py
@@ -2,8 +2,5 @@ import pandas as pd
 
 #car_accident = pd.read_csv('data/database_car_accident.csv')
 cities = pd.read_csv('data/comuni_italiani.csv', sep=';')
-print(cities.info())
 
-for name in list(cities):
-    print(f'For the column {name} we have these unique values: ')
-    print(cities[name].unique())
+print(cities[cities['Comune'].isnull()])
