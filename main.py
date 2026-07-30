3499ca620d2ac130bb6ccc891732cdeff51d4426
Giovanni De Santis <desgiovanni1@gmail.com>
2026-07-30 11:08:11 +0200

    start preliminary analysis of second database

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-car_accident = pd.read_csv('data/database_car_accident.csv')
+#car_accident = pd.read_csv('data/database_car_accident.csv')
 cities = pd.read_csv('data/comuni_italiani.csv', sep=';')
 print(cities.info())
 
