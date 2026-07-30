da587952b4cb31a2e8e6b58a246fa537a5a69025
giovanni_laptop <desgiovanni1@gmail.com>
2026-07-30 12:51:30 +0200

    Preliminary analysis complete

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ import pandas as pd
 #car_accident = pd.read_csv('data/database_car_accident.csv')
 cities = pd.read_csv('data/comuni_italiani.csv', sep=';')
 
-print(cities[cities['Comune'].isnull()])
+print(cities.shape)
+
+#print(cities[cities['Comune'].isnull()])
