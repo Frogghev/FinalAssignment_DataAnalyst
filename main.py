2ddedb10b2b5f93c9eb174d9c3d3bfb1cc953d82
Frogghev <desgiovanni1@gmail.com>
2026-08-02 15:55:57 +0200

    added graphs to indicate the relations between car crashes and injuries

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
-word = 'Superissimoooo'
-word = word[:-3]
-print(word)
+import numpy as np
+import pandas as pd
+from scipy import stats
+
+
+complete_dataset = pd.read_csv('data/complete_dataset.csv')
+
+cities_with_most_incidents = complete_dataset[complete_dataset['Anno']==2020][complete_dataset['DATA_TYPE']=='ROADACC']
+cities_with_most_incidents.sort_values(by=['OBS_VALUE'], ascending=False)
+
+# fit a simple linear regression manually
+slope, intercept, r, p, se = stats.linregress(
+    cities_with_most_incidents['Popolazione legale'],
+    cities_with_most_incidents['OBS_VALUE']
+)
+
+predicted = slope * cities_with_most_incidents['Popolazione legale'] + intercept
+residuals = cities_with_most_incidents['OBS_VALUE'] - predicted
+
+# grab the 6 with the largest absolute residual
+outliers = cities_with_most_incidents.reindex(
+    residuals.abs().sort_values(ascending=False).index
+).head(6)
+
+print(outliers)
