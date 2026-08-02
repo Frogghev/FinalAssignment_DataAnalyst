a8e7058b2e209475b741c815a04790e6e7c17e5c
Frogghev <desgiovanni1@gmail.com>
2026-08-02 12:40:19 +0200

    Fixed area type

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-import pandas as pd
-
-#car_accident = pd.read_csv('data/database_car_accident.csv')
-cities = pd.read_csv('data/comuni_italiani_2020.csv', sep=';')
-
-#fix Null value in Comune column
-comune_missing_value = cities[cities['Comune'].isnull()]
-comune_missing_value['Comune'] = 'Torino'
-
-#fix Null value in Cities column
-sigla_missing_value = cities[cities['Sigla automobilistica'].isnull()]
-sigla_missing_value['Sigla automobilistica'] = "NA"
-
-
+word = 'Superissimoooo'
+word = word[:-3]
+print(word)
